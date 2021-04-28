c9eb7f15c054c8c542fd50cac3904d5aeb490736
KP <kp@cornerstonews>
2021-04-28 08:22:38 -0700

    Updated Web Socket to work with mew cornerstone-adb-driver

--- src/main/java/com/github/cornerstonews/adb/websocket/AdminAdbWebsocket.java
+++ src/main/java/com/github/cornerstonews/adb/websocket/AdminAdbWebsocket.java
@@ -3,7 +3,6 @@ package com.github.cornerstonews.adb.websocket;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
@@ -14,8 +13,9 @@ import org.apache.logging.log4j.Logger;
 
 import com.github.cornerstonews.adb.AdbExecutor;
 import com.github.cornerstonews.adb.AdbManager;
+import com.github.cornerstonews.adb.CornerstoneADBException;
+import com.github.cornerstonews.adb.DeviceDO;
 import com.github.cornerstonews.adb.websocket.message.AdbDevicesGetMessage;
-import com.github.cornerstonews.adb.websocket.message.AdbDevicesGetMessage.DeviceStatus;
 import com.github.cornerstonews.adb.websocket.message.AdbDirectoryGetMessage;
 import com.github.cornerstonews.adb.websocket.message.AdbRebootMessage;
 import com.github.cornerstonews.adb.websocket.message.AdbShellCommandMessage;
@@ -29,19 +29,15 @@ public abstract class AdminAdbWebsocket extends AdbWebsocket {
 
     private static final Logger LOG = LogManager.getLogger(AdminAdbWebsocket.class);
 
-    private Map<String, AdbExecutor> devices;
-    
     public AdminAdbWebsocket(AdbManager adbManager) {
         super(adbManager);
-        this.devices = this.adbManager.getFilteredDevices(null);
     }
 
-    
     @OnMessage
     public void onMessage(Session session, String message, boolean isLast) {
         LOG.info("Received a string message.");
         LOG.debug("Message received: {}", message);
-        
+
         AdbWebsocketMessage adbMessage;
         try {
             adbMessage = JAXBUtils.unmarshalFromJSON(message, AdbWebsocketMessage.class);
@@ -57,18 +53,20 @@ public abstract class AdminAdbWebsocket extends AdbWebsocket {
             return;
         }
 
+        LOG.info("Message Type: {}", adbMessage.getMessageType());
         try {
-        	if (AdbWebsocketMessageType.DEVICES_GET == adbMessage.getMessageType()) {
-        		adbMessage = JAXBUtils.unmarshalFromJSON(message, AdbDevicesGetMessage.class);
-        		handleDevicesGet((AdbDevicesGetMessage) adbMessage, session);
+            if (AdbWebsocketMessageType.DEVICES_GET == adbMessage.getMessageType()) {
+                adbMessage = JAXBUtils.unmarshalFromJSON(message, AdbDevicesGetMessage.class);
+                handleDevicesGet((AdbDevicesGetMessage) adbMessage, session);
                 return;
             }
-        	
-        	this.deviceAdbExecutor = getDeviceAdbExecutor(adbMessage.getDeviceSerial());
-            if (deviceAdbExecutor == null) {
-                sendError("Authentication Failure. Device not found.", adbMessage, session);
-            }
 
+            AdbExecutor deviceAdbExecutor = this.adbManager.getDevice(adbMessage.getDeviceSerial());
+            if (deviceAdbExecutor == null) {
+                sendError("Device not found.", adbMessage, session);
+                return;
+            }
+            this.deviceSerial = adbMessage.getDeviceSerial();
 
             switch (adbMessage.getMessageType()) {
 
@@ -94,32 +92,19 @@ public abstract class AdminAdbWebsocket extends AdbWebsocket {
 
                 default:
                     sendError("Unknown ADB command.", adbMessage, session);
-                    return;
+                    break;
             }
-        } catch (JAXBException | IOException e) {
+        } catch (JAXBException | IOException | CornerstoneADBException e) {
             LOG.error("Error processing message for device serial '{}', Error: {}", adbMessage.getDeviceSerial(), e.getMessage(), e);
             sendError("Error processing message. Error: " + e.getMessage(), adbMessage, session);
             return;
         }
     }
-    
-    private void handleDevicesGet(AdbDevicesGetMessage adbMessage, Session session) throws IOException, JAXBException {
-    	List<DeviceStatus> devicesWithStatus = new ArrayList<DeviceStatus>();
-    	this.adbManager.getFilteredDevices(null).entrySet().parallelStream()
-    		.forEach((entry) -> devicesWithStatus.add(new DeviceStatus(entry.getKey(), entry.getValue().isOnline() ? "online" : "offline")));   	
-    	adbMessage.setDevices(devicesWithStatus);
+
+    private void handleDevicesGet(AdbDevicesGetMessage adbMessage, Session session) throws IOException, JAXBException, CornerstoneADBException {
+        List<DeviceDO> devices = new ArrayList<DeviceDO>();
+        this.adbManager.getDevices().forEach((entry) -> devices.add(entry.getDeviceInfo()));
+        adbMessage.setDevices(devices);
         this.sendSuccess(200, "Devices get successful.", adbMessage, session);
     }
-    
-    private AdbExecutor getDeviceAdbExecutor(String deviceSerial) {   	
-    	this.deviceAdbExecutor = this.devices.get(deviceSerial);
-    	
-    	if(this.deviceAdbExecutor == null) {
-        	this.devices = this.adbManager.getFilteredDevices(null);
-        	this.deviceAdbExecutor = this.devices.get(deviceSerial);
-    	}
-    	
-    	return this.deviceAdbExecutor;
-    }
-
 }
